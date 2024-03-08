240c23401f92cd078900ba400a34129115ddc0e1
nguyen452 <nguyen452@gmail.com>
2024-03-08 12:14:40 -0800

    Refactor dateParser function to handle both "/" and "-" separators

--- utility/dateParser.js
+++ utility/dateParser.js
@@ -1,6 +1,13 @@
 module.exports = function dateParser(date) {
-    //takes a format of " MM/DD/YYYY and returns YYYY-MM-DD"
-    let dateArr = date.split('/');
+    // takes a format of " MM/DD/YYYY or MM-DD-YYYY and returns YYYY-MM-DD"
+    let dateArr;
+
+    if (date[2] === "/") {
+        dateArr = date.split("/");
+    } else if (date[2] === "-") {
+        dateArr = date.split("-");
+    }
+
     dateArr = [dateArr[2], dateArr[0], dateArr[1]];
-    return dateArr.join('-');
+    return dateArr.join("-");
 };
