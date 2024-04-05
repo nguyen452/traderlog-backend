036cef827db23ad9e6ee4b2f241755907e186514
nguyen452 <nguyen452@gmail.com>
2024-04-05 13:34:46 -0700

    Refactor reportRoutes.js and add filtered statistic endpoint

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const passport = require("passport");
 const reportRouter = express.Router();
-const { addTradesToTracker } = require("../utility/tradeCalculation");
 const { Trade } = require("../db/models");
 const getAllModelDataByUserId = require("../utility/getAllModelDataByUserId");
 const TradeAnalyzer = require("../utility/tradeAnalyzer");
-const roundingNumbers = require("../utility/roundingNumbers");
+const calculateReportStatistic = require("../utility/calculateReportStatistic");
+const { Op } = require("sequelize");
 
 reportRouter.use(passport.authenticate("jwt", { session: false }));
 
@@ -28,18 +28,66 @@ reportRouter.get("/allStatisticData", async (req, res) => {
         await Promise.all(executionsPromises);
 
         //trade reports data
-        const totalGrossProfit = tradesAnalyzer.getTotalGrossProfit();
-        const averageDailyPnL = tradesAnalyzer.getAverageDailyPnl();
-        const averageDailyVolume = tradesAnalyzer.getAverageDailyVolume();
-        const averagePerSharePnL = roundingNumbers( tradesAnalyzer.getTotalGrossProfit()/tradesAnalyzer.getTotalVolumeTraded(), 2)
-        const averageWinningTrade = tradesAnalyzer.getAverageWin();
-        const averageLosingTrade = tradesAnalyzer.getAverageLoss();
-        const totalNumberOfTrades = tradesAnalyzer.trades.length;
-        const totalWinningTrades = tradesAnalyzer.getWinningTrades().length;
-        const totalLosingTrades = tradesAnalyzer.getLosingTrades().length;
-        // const maxConsecutiveWins = tradesAnalyzer.getMaxConsecutiveWins();
-        // const maxConsecutiveLosses = tradesAnalyzer.getMaxConsecutiveLosses();
-        res.status(200).json({ totalGrossProfit, averageDailyPnL, averageDailyVolume, averagePerSharePnL});
+       const statistic = calculateReportStatistic(tradesAnalyzer);
+       res.status(200).json(statistic);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+reportRouter.get("/statisticData/filtered", async (req, res) => {
+    let { startDate, endDate, symbol, duration } = req.query;
+    console.log(symbol, startDate, endDate, duration);
+    let queryCondition = {
+        user_id: req.user.id,
+    }
+    //symbol
+    if (symbol) {
+        queryCondition.symbol = symbol.toUpperCase();
+    }
+    //duration
+    if (duration !== "all" && duration === "intraday") {
+        queryCondition.date_start = date_close;
+    } else if (duration !== "all" && duration === "multiDay") {
+        queryCondition.date_start = { [Op.ne]: date_close };
+    }
+    //date
+
+    if (startDate && endDate) {
+        queryCondition.date_close = {
+            [Op.gte]: startDate,
+            [Op.lte]: endDate,
+        };
+    } else if (startDate) {
+        queryCondition.date_close = {
+            [Op.gte]: startDate,
+        };
+    }else if (endDate) {
+        queryCondition.date_close = {
+            [Op.lte]: endDate,
+        };
+    }
+
+    console.log(queryCondition)
+
+    try {
+        const tradeData = await Trade.findAll({where: queryCondition});
+
+        const tradesAnalyzer = new TradeAnalyzer();
+        const tradeIdArray = [];
+        tradeData.forEach((trade) => {
+            tradesAnalyzer.addTrade(trade);
+            tradeIdArray.push(trade.id);
+        });
+
+        const executionsPromises = tradeIdArray.map((tradeId) =>
+            tradesAnalyzer.addExecutionsByTradeId(tradeId)
+        );
+
+        await Promise.all(executionsPromises);
+
+        const statistic = calculateReportStatistic(tradesAnalyzer);
+        res.status(200).json(statistic);
     } catch (error) {
         console.log(error);
     }
